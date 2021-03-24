934cc015ff598c068d27047a12689db424e338f1
Βύρων Πασχαλίδης <pvyron@hotmail.gr>
2021-03-25 00:13:57 +0200

    Implement login functionality to desktop UI

--- PMDesktopUI/Bootstrapper.cs
+++ PMDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using PMDesktopUI.Helpers;
 using PMDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace PMDesktopUI
 {
@@ -16,6 +18,11 @@ namespace PMDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()
@@ -24,7 +31,8 @@ namespace PMDesktopUI
 
             _container
                 .Singleton<IWindowManager, WindowManager>()
-                .Singleton<IEventAggregator, EventAggregator>();
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IAPIHelper, APIHelper>();
 
             GetType().Assembly.GetTypes()
                 .Where(type => type.IsClass)
---
+++ PMDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using PMDesktopUI.Models;
+using System.Threading.Tasks;
+
+namespace PMDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
