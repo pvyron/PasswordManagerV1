e561c86df7ab1c43b580a4d541f7ec9f02d5cf76
Βύρων Πασχαλίδης <pvyron@hotmail.gr>
2021-03-26 02:21:36 +0200

    Change name of Entity Framework Database to EFDatabase

--- PMDataManager/Models/IdentityModels.cs
+++ PMDataManager/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace PMDataManager.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFDatabase", throwIfV1Schema: false)
         {
         }
         
